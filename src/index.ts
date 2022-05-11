415d40fc0124152f1f0c728d8e8825d6277198ba
soyocar <royomek@gmail.com>
2022-05-11 17:13:14 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+let btnEnv = document.getElementById("enviar");
+let altura: number = document.getElementById("dato");
+let alturapermitida: number = 1.3;
+
+btnEnv.addEventListener("click", () => {
+  let alturaper: number = Number(altura.value);
+  if (alturaper <= alturapermitida) {
+    console.log("La persona no puede subir al juego");
+  } else {
+    console.log("La persona puede subir al juego");
+  }
+
+  //}
+});
